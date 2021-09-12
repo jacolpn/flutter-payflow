2f0bd9c9aa6807dbf71bf90742cff912ad64326c
jacolpn <jacolpn@outlook.com>
2021-09-11 23:00:48 -0300

    added shared preferences and finalized authentication

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:pageflow/shared/auth/auth_controller.dart';
+import 'package:pageflow/shared/models/user_model.dart';
 
 class LoginController {
   final authController = AuthController();
@@ -13,8 +14,10 @@ class LoginController {
       final response = await _googleSignIn.signIn();
       // ignore: avoid_print
       print(response);
+      final user =
+          UserModel(name: response!.displayName!, photoURL: response.photoUrl!);
 
-      authController.setUser(context, response);
+      authController.setUser(context, user);
     } catch (error) {
       // ignore: avoid_print
       print(error);
--- lib/shared/auth/auth_controller.dart
+++ lib/shared/auth/auth_controller.dart
@@ -1,30 +1,54 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:pageflow/modules/home/home_page.dart';
-import 'package:pageflow/modules/login/login_page.dart';
+import 'package:pageflow/shared/models/user_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthController {
-  // ignore: unused_field
-  var _isAuthenticated = false;
-  // ignore: prefer_typing_uninitialized_variables
-  var _user;
+  // var _isAuthenticated = false;
+  UserModel? _user;
 
-  get user => _user;
+  UserModel get user => _user!;
 
-  void setUser(BuildContext context, var user) {
+  void setUser(BuildContext context, UserModel? user) {
     if (user != null) {
+      saveUser(user);
+
       _user = user;
-      _isAuthenticated = true;
+      // _isAuthenticated = true;
 
       // Navigator.pushReplacement(
       //     context, MaterialPageRoute(builder: (context) => const HomePage()));
       Navigator.pushReplacementNamed(context, '/home');
     } else {
-      _isAuthenticated = false;
+      // _isAuthenticated = false;
 
       // Navigator.push(
       //     context, MaterialPageRoute(builder: (context) => const LoginPage()));
       Navigator.pushReplacementNamed(context, '/login');
+    }
+  }
+
+  Future<void> saveUser(UserModel user) async {
+    final instance = await SharedPreferences.getInstance();
+
+    await instance.setString("user", user.toJson());
+
+    return;
+  }
+
+  Future<void> currentUser(BuildContext context) async {
+    final instance = await SharedPreferences.getInstance();
+
+    await Future.delayed(const Duration(seconds: 2));
+
+    if (instance.containsKey("user")) {
+      final json = instance.get("user") as String;
+
+      setUser(context, UserModel.fromJson(json));
+
+      return;
+    } else {
+      setUser(context, null);
     }
   }
 }
